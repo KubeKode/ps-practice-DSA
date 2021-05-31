2d5244e9aaa3b52ca65ed27d384cc9398ab65e09
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-05-31 13:05:23 +0000

    recursion sum of digits

--- dsa-practice/recursion-backtracking/intro/sum.js
+++ dsa-practice/recursion-backtracking/intro/sum.js
@@ -1,10 +1,10 @@
-const sum= (n)=>{
-    if(n===1){
+var sum = function (num) {
+    if (num === 1) {
         return 1;
     }
-    else if(n===0){
+    else if (num === 0) {
         return 0;
     }
-    return n + sum(n-1)
-}
-console.log(sum(4))
+    return num + sum(num - 1);
+};
+console.log(sum(5));
